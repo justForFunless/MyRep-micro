b382a93b3c7b2e1b0ad9273492bf09424db14241
jianghao <jianghao@ysstech.com>
2018-10-31 16:28:18 +0800

    修改

--- cluster_test/battcn-cloud-h1/src/main/java/com/battcn/controller/H1Controller.java
+++ cluster_test/battcn-cloud-h1/src/main/java/com/battcn/controller/H1Controller.java
@@ -1,4 +1,23 @@
 package com.battcn.controller;
 
+import com.battcn.H1Application;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class H1Controller {
+    static Logger LOGGER = LoggerFactory.getLogger(H1Application.class);
+
+    @Value("${spring.application.name}")
+    String applicationName;
+
+    @RequestMapping("/hello")
+    public String typeInfo(@RequestParam String email) {
+        LOGGER.info("[email] - [{}]",email);
+        return "My Name's :" + applicationName + " Email:" + email;
+    }
 }
--- cluster_test/battcn-cloud-h2/src/main/java/com/battcn/controller/H2Controller.java
+++ cluster_test/battcn-cloud-h2/src/main/java/com/battcn/controller/H2Controller.java
@@ -1,4 +1,29 @@
 package com.battcn.controller;
 
+import com.battcn.H2Application;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+@RestController
 public class H2Controller {
+
+    static Logger LOGGER = LoggerFactory.getLogger(H2Application.class);
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+
+    @RequestMapping("/hello2")
+    public String home(@RequestParam String email) {
+        // 改写法之前有说过是VIP模式
+        return restTemplate().getForObject("http://localhost/hello?email=" + email, String.class);
+    }
 }
